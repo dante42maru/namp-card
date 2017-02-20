ef158498db50d5c5dc0853c530c73baed2217971
ragingwind <ragingwind@gmail.com>
2017-02-20 13:35:20 +0900

    Using firebase static hosting instead of github.io

--- src/sw.js
+++ src/sw.js
@@ -1,8 +1,8 @@
 // sw.js
 
-var CACHE_NAME = 'pwa-workshop.github.id-namp-card-cache-v1';
+var CACHE_NAME = 'namp-card.firebaseapp.com-cache-v1';
 var urlsToCache = [
-	'/namp-card'
+	'/'
 ];
 
 self.addEventListener('install', function(event) {
